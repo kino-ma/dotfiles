9e1e9ce771cfed4ef3664b7a1233a6265b565b29
kino-ma <kino.ma.ms@gmail.com>
2020-10-26 16:02:20 +0900

    fix typo

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 cp -r .zshrc .vimrc .vim ~/ \
     && curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh \
     && sh ./installer.sh ~/.vim/dein \
-    && rm insttaller.sh \
+    && rm installer.sh \
