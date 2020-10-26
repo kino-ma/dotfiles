93d81b96583300edbd3be61c2a2d3f547115e531
kino-ma <kino.ma.ms@gmail.com>
2020-10-26 16:00:36 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
-cp -r .zshrc .vimrc .vim ~/
-
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.vim/dein
-rm insttaller.sh
+cp -r .zshrc .vimrc .vim ~/ \
+    && curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh \
+    && sh ./installer.sh ~/.vim/dein \
+    && rm insttaller.sh \
