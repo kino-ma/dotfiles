46733dccc3712a8f58157c9adbe081edb4125b02
kino-ma <kino.ma.ms@gmail.com>
2020-10-26 17:40:38 +0900

    install GitHub CLI

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #! /bin/sh
 cp -r .zshrc .vimrc .vim ~/ \
-    && cp -r nvim ~/.config/nvim \
-    && mkdir ~/.zsh \
-    && curl https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.zsh > ~/.zsh/git-flow-completion.zsh \
+    && cp -r nvim ~/.config/nvim
     && curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh \
     && sh ./dein_install.sh ~/.vim/dein 1>/dev/null \
-    && rm dein_install.sh \
+    && rm dein_install.sh
+
+mkdir ~/.zsh \
+    && curl https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.zsh > ~/.zsh/git-flow-completion.zsh
+
+sudo apt-get update \
+    && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0 \
+    && sudo apt-add-repository https://cli.github.com/packages \
+    && sudo apt update \
+    && sudo apt install gh
