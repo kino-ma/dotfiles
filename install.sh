38c336b3a6d4c0783015e21d028f14d03253c449
kino-ma <kino.ma.ms@gmail.com>
2020-10-26 18:10:07 +0900

    check apt existance

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #! /bin/sh
+
+if which apt 1>/dev/null
+then
+    sudo apt update \
+        && sudo apt install -y git curl vim neovim
+
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0 \
+        && sudo apt-add-repository https://cli.github.com/packages \
+        && sudo apt update \
+        && sudo apt install -y gh
+fi
+
 cp -r .zshrc .vimrc .vim ~/ \
     && cp -r nvim ~/.config/nvim \
     && curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh \
@@ -7,9 +19,3 @@ cp -r .zshrc .vimrc .vim ~/ \
 
 mkdir ~/.zsh \
     && curl https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.zsh > ~/.zsh/git-flow-completion.zsh
-
-sudo apt-get update \
-    && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0 \
-    && sudo apt-add-repository https://cli.github.com/packages \
-    && sudo apt update \
-    && sudo apt install gh
