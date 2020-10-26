b465ca10cec3d2b93c1c45b9779d44600370321e
kino-ma <kino.ma.ms@gmail.com>
2020-10-26 16:18:59 +0900

    do not display dein output

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 cp -r .zshrc .vimrc .vim ~/ \
     && cp -r nvim ~/.config/nvim \
     && curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh \
-    && sh ./dein_install.sh ~/.vim/dein \
+    && sh ./dein_install.sh ~/.vim/dein 1>/dev/null \
     && rm dein_install.sh \
