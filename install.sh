ae5ed5e3fe6fb4b6e934367d597a76911f526a3d
kino-ma <ma@kino.ma>
2022-01-09 09:39:56 +0000

    [fix] wrong quotation

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ install_hub() {
 
 install_dein() {
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh \
-        && sh ./dein_install.sh "$HOME/.vim/dein 1>/dev/null" \
+        && sh ./dein_install.sh "$HOME/.vim/dein" 1>/dev/null \
         && rm dein_install.sh
 }
 
