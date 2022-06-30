f47c0fdfd98ef06ff1db53366450c5258f1e9fa8
kino-ma <ma@kino.ma>
2022-06-30 16:58:23 +0900

    [fix] prevent debug output

--- .zsh/gpg.zsh
+++ .zsh/gpg.zsh
@@ -1,8 +1,7 @@
-# If this session itself is a SSH session
 if [[ ( -n "$SSH_CONNECTION" ) && ( -n "$SSH_AUTH_SOCK" ) ]]
 then
-    echo "SSH_AUTH_SOCK: $SSH_AUTH_SOCK"
-    # SSH_AUTH_SOCK is already set
+    # If this session itself is a SSH session
+    # So SSH_AUTH_SOCK is already set by SSH Agent. Nothing to do here
 else
     export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
