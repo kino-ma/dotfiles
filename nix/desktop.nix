bec4c94de81bd452f1fb91b3dcbeee3e9b63be07
kino-ma <ma@kino.ma>
2022-08-01 06:36:51 +0900

    [feat] add dconf to move cursor to center of window

--- nix/desktop.nix
+++ nix/desktop.nix
@@ -9,6 +9,15 @@
     tldr
     vscode
     xclip
+    xdotool
     zoom
   ]);
+
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Control><Alt>a";
+      command ="/home/kino-ma/dotfiles/scripts/move-cursor-center.sh";
+      name = "Move curosr to the center of active window";
+    };
+  };
 }
