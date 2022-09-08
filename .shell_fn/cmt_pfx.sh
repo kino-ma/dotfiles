6616b195db49ed41e258ac417a34e93334ccec98
kino-ma <ma@kino.ma>
2022-09-08 10:17:32 +0900

    fix: use `type: ` instead of `[type] ` for commit prefixes

--- .shell_fn/cmt_pfx.sh
+++ .shell_fn/cmt_pfx.sh
@@ -2,5 +2,5 @@ git_commit_with_prefix() {
     GIT_PREFIX=$1
     GIT_COMMIT_MESSAGE=$2
     shift 2
-    git commit -m "[$GIT_PREFIX] $GIT_COMMIT_MESSAGE" "$@"
+    git commit -m "${GIT_PREFIX}: ${GIT_COMMIT_MESSAGE}" "$@"
 }
