ebb73547a8b982fc25e1a1c5a49e81d511feeb38
kino-ma <ma@kino.ma>
2023-06-16 17:14:57 +0900

    fix: syntax error

--- install.sh
+++ install.sh
@@ -143,6 +143,7 @@ install_dein() {
     # curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_install.sh
     # sh ./dein_install.sh "$HOME/.vim/dein" 1>/dev/null
     # rm dein_install.sh
+    :
 }
 
 install_gitflow() {
