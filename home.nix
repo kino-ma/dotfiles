1e42ca58f3733babee5665cf74f6f739fcade24f
kino-ma <ma@kino.ma>
2024-02-06 19:42:54 +0900

    refactor: split programs configuration and apply formatter

--- home.nix
+++ home.nix
@@ -1,12 +1,14 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 {
-  home.stateVersion = "24.05"; 
+  home.stateVersion = "24.05";
   home.packages = with pkgs;
-    [ git
+    [
+      git
       gnupg
       home-manager
       iterm2
+      nixpkgs-fmt
       pinentry_mac
       vscode
 
@@ -17,71 +19,15 @@
       zoom-us
     ];
 
-    home.sessionVariables = {
-      SSH_AUTH_SOCK = "$HOME/.gnupg/S.gpg-agent.ssh";
-    };
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "$HOME/.gnupg/S.gpg-agent.ssh";
+  };
 
   programs.home-manager.enable = true;
 
   programs.neovim = import ./neovim.nix;
-
-  # Git settings
-  programs.git =
-    { enable = true;
-      userName = "kino-ma";
-      userEmail = "ma@kino.ma"; 
-      signing =
-        { key = "F0D7A2F02CAE648A";
-          signByDefault = true;
-        };
-
-      extraConfig =
-        ''
-          [pull]
-          ff = only
-
-          [init]
-          defaultBranch = true
-        '';
-
-      aliases =
-        { c = "commit -m";
-          cn = "commit --no-gpg-sign -m";
-          s = "status";
-          d = "diff";
-          ds = "diff --staged";
-
-          c-feat = ''!"${./shell-helpers/commit-with-prefix.sh} feat"'';
-          c-fix = ''!"${./shell-helpers/commit-with-prefix.sh} fix"'';
-          c-refactor = ''!"${./shell-helpers/commit-with-prefix.sh} refactor"'';
-          c-test = ''!"${./shell-helpers/commit-with-prefix.sh} test"'';
-          c-perf = ''!"${./shell-helpers/commit-with-prefix.sh} perf"'';
-          c-style = ''!"${./shell-helpers/commit-with-prefix.sh} style"'';
-          c-docs = ''!"${./shell-helpers/commit-with-prefix.sh} docs"'';
-          c-build = ''!"${./shell-helpers/commit-with-prefix.sh} build"'';
-          c-ci = ''!"${./shell-helpers/commit-with-prefix.sh} ci"'';
-
-          force-clean = "clean -fdX";
-          gr = "log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'";
-          m-init = "submodule update --init --recursive";
-        };
-
-      ignores =
-      	[
-          "*.*~"
-          "*.swp"
-          ".vscode"
-          ".DS_Store"
-        ];
-    };
-
-  # GnuPG settings
-  programs.gpg =
-    { enable = true;
-      publicKeys =
-        [ { source = ./keys/gpg/pubkey.txt; trust = "ultimate"; }
-        ];
-    };
+  programs.git = import ./programs/git.nix;
+  programs.gpg = import ./programs/gpg.nix;
   home.file.".gnupg/gpg-agent.conf".text =
     ''
       pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
---
+++ programs/git.nix
@@ -0,0 +1,50 @@
+{
+  enable = true;
+  userName = "kino-ma";
+  userEmail = "ma@kino.ma";
+  signing =
+    {
+      key = "F0D7A2F02CAE648A";
+      signByDefault = true;
+    };
+
+  extraConfig =
+    ''
+      [pull]
+      ff = only
+
+      [init]
+      defaultBranch = true
+    '';
+
+  aliases =
+    {
+      c = "commit -m";
+      cn = "commit --no-gpg-sign -m";
+      s = "status";
+      d = "diff";
+      ds = "diff --staged";
+
+      c-feat = ''!"${../shell-helpers/commit-with-prefix.sh} feat"'';
+      c-fix = ''!"${../shell-helpers/commit-with-prefix.sh} fix"'';
+      c-refactor = ''!"${../shell-helpers/commit-with-prefix.sh} refactor"'';
+      c-test = ''!"${../shell-helpers/commit-with-prefix.sh} test"'';
+      c-perf = ''!"${../shell-helpers/commit-with-prefix.sh} perf"'';
+      c-style = ''!"${../shell-helpers/commit-with-prefix.sh} style"'';
+      c-docs = ''!"${../shell-helpers/commit-with-prefix.sh} docs"'';
+      c-build = ''!"${../shell-helpers/commit-with-prefix.sh} build"'';
+      c-ci = ''!"${../shell-helpers/commit-with-prefix.sh} ci"'';
+
+      force-clean = "clean -fdX";
+      gr = "log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'";
+      m-init = "submodule update --init --recursive";
+    };
+
+  ignores =
+    [
+      "*.*~"
+      "*.swp"
+      ".vscode"
+      ".DS_Store"
+    ];
+}
