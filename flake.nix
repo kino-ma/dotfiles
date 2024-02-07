52cba43bf00982c2990bd57700b6a6f4c276979c
kino-ma <ma@kino.ma>
2024-02-07 13:42:28 +0900

    style: apply formatter

--- flake.nix
+++ flake.nix
@@ -6,34 +6,34 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager = 
-      { url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs-unstable";
-      };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nixpkgs-unstable, home-manager }:
 
-let
-  darwin-config = import ./darwin-common.nix;
+    let
+      darwin-config = import ./darwin-common.nix;
 
-in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#babylon
-    darwinConfigurations."babylon" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules =
-        [ darwin-config
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#babylon
+      darwinConfigurations."babylon" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          darwin-config
           home-manager.darwinModules.home-manager
-            { home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-            }
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
         ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."babylon".pkgs;
     };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."babylon".pkgs;
-
-  };
 }
