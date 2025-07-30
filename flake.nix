5601017f8e3a57e3ec0a4d72bf9a26d423af5eaf
kino-ma <ma@kino.ma>
2025-07-30 16:23:01 +0900

    chore: fix to 25.05

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   description = "Example Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/25.05-pre";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     dotfiles-private = {
