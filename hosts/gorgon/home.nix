aa4734662c6be78ce1e06abfb6a004a4d79da49d
kino-ma <ma@kino.ma>
2025-09-01 18:50:10 +0900

    feat: configure localstack for gorgon

--- hosts/gorgon/home.nix
+++ hosts/gorgon/home.nix
@@ -7,4 +7,23 @@
     ''
       source ${../../.vim/rc/vscode.vim}
     '';
+
+  programs.awscli = {
+    enable = true;
+
+    settings = {
+      localstack = {
+        region = "ap-northeast-1";
+        output = "json";
+        endpoint_url = "http://localhost:4566";
+      };
+    };
+
+    credentials = {
+      localstack = {
+        aws_access_key = "test";
+        aws_secret_access_key = "test";
+      };
+    };
+  };
 }
