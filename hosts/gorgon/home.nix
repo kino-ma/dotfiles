0602930dbfa43b739ead18cfec81a659daf84339
kino-ma <ma@kino.ma>
2025-09-04 15:59:30 +0900

    fix: valid aws config

--- hosts/gorgon/home.nix
+++ hosts/gorgon/home.nix
@@ -12,7 +12,7 @@
     enable = true;
 
     settings = {
-      localstack = {
+      "profile localstack" = {
         region = "ap-northeast-1";
         output = "json";
         endpoint_url = "http://localhost:4566";
@@ -21,7 +21,7 @@
 
     credentials = {
       localstack = {
-        aws_access_key = "test";
+        aws_access_key_id = "test";
         aws_secret_access_key = "test";
       };
     };
