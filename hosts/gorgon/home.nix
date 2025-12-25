f92d52767858aed332e16697d7746393d32f2ea2
kino-ma <ma@kino.ma>
2025-12-25 18:11:17 +0900

    refactor: move awscli config to private

--- hosts/gorgon/home.nix
+++ hosts/gorgon/home.nix
@@ -7,23 +7,4 @@
     ''
       source ${../../.vim/rc/vscode.vim}
     '';
-
-  programs.awscli = {
-    enable = true;
-
-    settings = {
-      "profile localstack" = {
-        region = "ap-northeast-1";
-        output = "json";
-        endpoint_url = "http://localhost:4566";
-      };
-    };
-
-    credentials = {
-      localstack = {
-        aws_access_key_id = "test";
-        aws_secret_access_key = "test";
-      };
-    };
-  };
 }
